385469f7a4492f2b3afa327c6f51c2fd499fde33
Noel <noel@2bedigital.com>
2017-06-30 08:44:12 +0100

    remove order to Json

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -2,23 +2,6 @@ module Spree
   module Admin
     Spree::Admin::OrdersController.class_eval do
 
-      require 'httparty'
-
-      def json_from_api
-        if try_spree_current_user && spree_current_user.spree_api_key.present?
-          result = HTTParty.get(
-              'https://' + Spree::Store.first.url+'/api/orders/'+load_order.number,
-              #'/api/orders/'+load_order.number,
-              verify: false,
-              headers: { "X-Spree-Token" =>   spree_current_user.spree_api_key}
-          ).parsed_response
-          render :json => result
-        else
-          flash[:error] = Spree.t(:authorization_failure)
-          redirect_to spree.admin_unauthorized_path
-        end
-      end
-
       def destroy
         order = Spree::Order.find(params[:id])
         if !order.complete?
--- app/overrides/spree/admin/orders/index/layouts_admin_json_export_button_decorator.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(virtual_path: "spree/admin/shared/_header",
-                     name: "json_export_button",
-                     insert_top: "ul[data-hook='toolbar']",
-                     partial: "spree/admin/orders/add_json_order_export_button",
-                     disabled: false)
