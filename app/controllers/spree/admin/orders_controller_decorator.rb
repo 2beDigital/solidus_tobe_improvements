5781f0b467c3cb7ef1641f5b7d8ab845cbdcfc70
Noel <noel@2bedigital.com>
2017-08-07 17:14:09 +0100

    bug in resend confirm_email, PROY-172

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,21 +1,24 @@
-module Spree
-  module Admin
-    Spree::Admin::OrdersController.class_eval do
+Spree::Admin::OrdersController.class_eval do
 
-      def destroy
-        order = Spree::Order.find(params[:id])
-        if !order.complete?
-          order.delete
-          flash[:success] = I18n.t(:order_delete)
-          respond_with(order) do |format|
-            format.html { redirect_to :back }
-            format.js   { render_js_for_destroy }
-          end
-        else
-           flash[:notice] = I18n.t(:order_not_delete)   
-           redirect_to :back      
-        end
+  def resend
+    OrderMailer.confirm_email(@order, true).deliver_later
+    flash[:success] = Spree.t(:order_email_resent)
+
+    redirect_to(spree.edit_admin_order_path(@order))
+  end
+
+  def destroy
+    order = Spree::Order.find(params[:id])
+    if !order.complete?
+      order.delete
+      flash[:success] = I18n.t(:order_delete)
+      respond_with(order) do |format|
+        format.html { redirect_to :back }
+        format.js   { render_js_for_destroy }
       end
+    else
+       flash[:notice] = I18n.t(:order_not_delete)   
+       redirect_to :back      
     end
   end
 end
