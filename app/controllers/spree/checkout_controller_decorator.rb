410f7831caf75bcf55618b6e11e9bbc6e9608add
Noel <noel@2bedigital.com>
2018-07-31 10:24:13 +0100

    bug in checkout controller

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::CheckoutController.class_eval do
   def ensure_checkout_allowed
     check = @order.checkout_allowed?
     unless check == true
-      redirect_to spree.root_path, :flash => {:notice => I18n.t("checkout_allowed_errors.#{check}") + Spree::Config[:minimum_order_value] + " " + Spree::Config[:currency]}
+      redirect_to spree.root_path, :flash => {:notice => I18n.t("checkout_allowed_errors.#{check}") + Spree::Config[:minimum_order_value].to_s + " " + Spree::Config[:currency]}
     end
   end
 
