618875ce2b6ce0de426cdcfc92beeb8405bfeb0d
Noel <noel@2bedigital.com>
2018-10-22 10:30:32 +0100

    bug in confirm refund with bank transfer

--- app/models/spree/check_decorator.rb
+++ app/models/spree/check_decorator.rb
@@ -1,5 +1,8 @@
 Spree::PaymentMethod::Check.class_eval do
   def cancel(*)
-    '0666'
-  end
+		ActiveMerchant::Billing::Response.new(true, '666: you must refund manually from tpv admin', {}, {})
+	end
+  def credit(amount_in_cents, auth_code, gateway_options = {})
+	  ActiveMerchant::Billing::Response.new(true, '666: you must refund manually from tpv admin', {}, {})
+	end
 end
